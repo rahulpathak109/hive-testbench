71b4c7b5abd188c50383678cac2f12431c91557a
rp <rahulpathak109@gmail.com>
2022-01-21 19:34:09 -0800

    Added hadoop version option and updated maven binary

--- tpch-build.sh
+++ tpch-build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Check for all the stuff I need to function.
-for f in gcc javac; do
+for f in gcc javac unzip; do
 	which $f > /dev/null 2>&1
 	if [ $? -ne 0 ]; then
 		echo "Required program $f is missing. Please install or fix your path and try again."
@@ -13,26 +13,38 @@ done
 which mvn > /dev/null 2>&1
 if [ $? -ne 0 ]; then
 	SKIP=0
-	if [ -e "apache-maven-3.0.5-bin.tar.gz" ]; then
-		SIZE=`du -b apache-maven-3.0.5-bin.tar.gz | cut -f 1`
-		if [ $SIZE -eq 5144659 ]; then
+	if [ -e "apache-maven-3.8.4-bin.tar.gz" ]; then
+		SIZE=`du -b apache-maven-3.8.4-bin.tar.gz | cut -f 1`
+		if [ $SIZE -eq 9046177 ]; then
 			SKIP=1
 		fi
 	fi
 	if [ $SKIP -ne 1 ]; then
 		echo "Maven not found, automatically installing it."
-		curl -O https://downloads.apache.org/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz 2> /dev/null
+		curl -O https://downloads.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz 2> /dev/null
 		if [ $? -ne 0 ]; then
 			echo "Failed to download Maven, check Internet connectivity and try again."
 			exit 1
 		fi
 	fi
-	tar -zxf apache-maven-3.0.5-bin.tar.gz > /dev/null
+	tar -zxf apache-maven-3.8.4-bin.tar.gz > /dev/null
 	CWD=$(pwd)
 	export MAVEN_HOME="$CWD/apache-maven-3.0.5"
 	export PATH=$PATH:$MAVEN_HOME/bin
+	if [ x$HADOOP_VERSION == x ]; then
+		echo "Please set your hadoop version"
+		echo "example: run 'hadoop version' and use first 3 number from the output"
+                echo "hadoop version\nHadoop 3.1.1.3.1.0.0-78\nexport HADOOP_VERSION=3.1.1"
+
+		exit 1
+	fi
 fi
 
 echo "Building TPC-H Data Generator"
 (cd tpch-gen; make)
-echo "TPC-H Data Generator built, you can now use tpch-setup.sh to generate data."
+
+if [ $? -ne 0 ]; then
+	echo "ERROR: TPC-H Data Generator built failed!!"
+else
+	echo "TPC-H Data Generator built, you can now use tpch-setup.sh to generate data."
+fi
